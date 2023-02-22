b3dc339a23eb8c19e01e550eebc0f420dce1d32b
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-02-21 22:13:27 -0300

    :construction: Novo layout principal

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,58 +1,40 @@
 <template>
-  <q-layout view="hHh LpR fFf">
-    <q-drawer
-      v-model="leftDrawerOpen"
-      :width="200"
-      :breakpoint="700"
-      show-if-above
-      class="bg-blue-9"
-      :mini="true"
-    >
-      <q-item
-        v-for="link in routes[0].children.filter((route) => !!route.showMenu)"
-        :key="link.title"
-        class="fade item"
-        clickable
-        @click="$router.push(link.path)"
-      >
-        <EssentialLink v-bind="link" />
-      </q-item>
-    </q-drawer>
+  <q-layout view="hHh lpR fff">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title class="fade">
+          <q-avatar>
+          </q-avatar>
+          CliMacaé
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar class="row justify-center">
+        <div class="col-3 col-md-1 fade">
+          <q-toolbar-title>
+            <a target="_blank" href="https://www.linkedin.com/in/thiago-paes-de-carvalho/">
+              <q-btn unelevated round icon="ion-logo-linkedin" />
+            </a>
+            <a target="_blank" href="https://github.com/thiagocarvalho93">
+              <q-btn unelevated round icon="ion-logo-github" />
+            </a>
+          </q-toolbar-title>
+        </div>
+      </q-toolbar>
+    </q-footer>
+
   </q-layout>
 </template>
-
-<script>
-import { defineComponent, ref } from "vue";
-import EssentialLink from "components/EssentialLink.vue";
-import routes from "src/router/routes";
-
-export default defineComponent({
-  name: "MainLayout",
-
-  components: {
-    EssentialLink,
-  },
-
-  setup() {
-    const leftDrawerOpen = ref(false);
-
-    return {
-      routes: routes.filter((route) => !!route.showMenu),
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-
-      clickItem() {
-        this.$router.push(path);
-      },
-    };
-  },
-});
-</script>
-<style scoped>
+<style>
+a {
+  text-decoration: none;
+  color: white;
+}
 </style>
