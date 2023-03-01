cec1e82c28eb8073a61b780210ce01e705e87916
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-02-28 22:35:15 -0300

    🔥 Remoção de arquivos não utilizados

--- src/pages/VentoPage.vue
+++
@@ -1,4 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<template>
-  <q-page class="q-pa-md bg-blue-grey-1"></q-page>
-</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -13,33 +13,6 @@ const routes = [
         component: () => import("pages/IndexPage.vue"),
       },
       {
-        path: "temperatura",
-        title: "Temperatura",
-        icon: "thermostat",
-        showMenu: true,
-        component: () => import("pages/TemperaturaPage.vue"),
-      },
-      {
-        path: "precipitacao",
-        title: "Precipitação",
-        icon: "water_drop",
-        showMenu: true,
-        component: () => import("pages/PrecipitacaoPage.vue"),
-      },
-      {
-        path: "vento",
-        title: "Vento",
-        icon: "wind_power",
-        showMenu: true,
-        component: () => import("pages/VentoPage.vue"),
-      },
-      {
-        path: "pressao",
-        title: "Pressão",
-        icon: "compress",
-        component: () => import("pages/PressaoPage.vue"),
-      },
-      {
         path: "/:catchAll(.*)*",
         showMenu: false,
         component: () => import("pages/ErrorNotFound.vue"),
