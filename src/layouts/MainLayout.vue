ea55ac389cdba531b236d65cbfeedeb8fcef35c8
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-03-13 21:18:01 -0300

    💄 Mudança de cor do header e alteração layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,66 @@
 <template>
   <q-layout view="hHh lpR fff">
-
-    <q-header class="navbar text-white">
+    <q-header
+      :bordered="isDarkMode"
+      reveal
+      :class="`${isDarkMode ? 'bg-dark' : 'bg-white shadow-2'}`"
+    >
       <q-toolbar>
         <q-toolbar-title class="fade">
           <q-avatar>
-            <q-img src="../assets/logo-192x192.png" spinner-color="white" style="height: 30px;" fit="scale-down" />
+            <q-img
+              src="../assets/logo-192x192.png"
+              spinner-color="white"
+              style="height: 25px"
+              fit="scale-down"
+            />
           </q-avatar>
         </q-toolbar-title>
-        <q-toggle v-model="isDarkMode" color="dark" @update:model-value="toggleDarkMode(isDarkMode)" icon="nightlight"
-          unchecked-icon="light_mode" />
+        <q-toggle
+          v-model="isDarkMode"
+          color="black"
+          keep-color
+          @update:model-value="toggleDarkMode(isDarkMode)"
+          icon="nightlight"
+          unchecked-icon="light_mode"
+        />
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
-      <q-page-sticky expand position="top">
-        <q-toolbar :class="`${isDarkMode ? 'bg-dark' : 'bg-white'}`">
-          <q-toolbar-title class="text-h6"> {{ $route.name }}</q-toolbar-title>
-        </q-toolbar>
-      </q-page-sticky>
     </q-page-container>
 
-    <q-footer bordered :class="`text-white ${isDarkMode ? 'bg-dark' : 'bg-white'}`">
+    <q-footer
+      bordered
+      :class="`text-white ${isDarkMode ? 'bg-dark' : 'bg-white'}`"
+    >
       <q-toolbar class="row justify-center">
         <div class="col-3 col-md-1 fade">
           <q-toolbar-title>
-            <a target="_blank" href="https://www.linkedin.com/in/thiago-paes-de-carvalho/">
-              <q-btn round unelevated :text-color="isDarkMode ? 'white' : 'dark'" icon="ion-logo-linkedin" />
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/thiago-paes-de-carvalho/"
+            >
+              <q-btn
+                round
+                unelevated
+                :text-color="isDarkMode ? 'white' : 'dark'"
+                icon="ion-logo-linkedin"
+              />
             </a>
             <a target="_blank" href="https://github.com/thiagocarvalho93">
-              <q-btn round unelevated :text-color="isDarkMode ? 'white' : 'dark'" icon="ion-logo-github" />
+              <q-btn
+                round
+                unelevated
+                :text-color="isDarkMode ? 'white' : 'dark'"
+                icon="ion-logo-github"
+              />
             </a>
           </q-toolbar-title>
         </div>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 <style>
@@ -47,24 +71,23 @@ a {
 </style>
 
 <script>
-import { ref } from "vue"
+import { ref } from "vue";
 
 export default {
   setup() {
     return {
-      isDarkMode: ref(false)
-    }
+      isDarkMode: ref(false),
+    };
   },
 
-  mounted() {
-  },
+  mounted() {},
 
   methods: {
     toggleDarkMode(val) {
       this.$q.dark.set(val);
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style lang="sass">
 .navbar
