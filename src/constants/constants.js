1b2d4fe156ea02950a7c7490bad8ed5d26cc180d
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-08-18 20:24:03 -0300

    ✏️ Ajustando estações de Macaé

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,21 @@
 const STATIONS = {
-  IRIODA1: { ID: "IRIODA1", NOME: "RO - Alphaville" },
-  IRIODA5: { ID: "IRIODA5", NOME: "RO - Parque dos Pássaros" },
-  IRIODA6: { ID: "IRIODA6", NOME: "Canta Galo" },
+  // IRIODA1: { ID: "IRIODA1", NOME: "RO - Alphaville" },
+  // IRIODA5: { ID: "IRIODA5", NOME: "RO - Parque dos Pássaros" },
+  // IRIODA6: { ID: "IRIODA6", NOME: "Canta Galo" },
   IMACA6: { ID: "IMACA6", NOME: "Mirante" },
-  IMACA13: { ID: "IMACA13", NOME: "Trapiche" },
+  IMACA13: { ID: "IMACA13", NOME: "Serra" },
   IMACA15: { ID: "IMACA15", NOME: "Glória" },
   // IMACA23: { ID: "IMACA23", NOME: "" }, desativada
-  IMACA26: { ID: "IMACA26", NOME: "Glória 2" },
+  IMACA26: { ID: "IMACA26", NOME: "Granja dos Cavaleiros" },
   IMACA27: { ID: "IMACA27", NOME: "Imbetiba" },
   IMACA28: { ID: "IMACA28", NOME: "Aroeira" },
   IMACA30: { ID: "IMACA30", NOME: "Imboassica" },
-  IMACA31: { ID: "IMACA31", NOME: "Centro" },
+  IMACA31: { ID: "IMACA31", NOME: "Botafogo" },
   IMACA32: { ID: "IMACA32", NOME: "Visconde" },
-  IMACA36: { ID: "IMACA36", NOME: "Glicério" },
+  IMACA36: { ID: "IMACA36", NOME: "Córrego do Ouro" },
   // ICAMPO96: { ID: "ICAMPO96", NOME: "" }, desativada
   // ICAMPO223: { ID: "ICAMPO223", NOME: "" },
-  ICABOF4: { ID: "ICABOF4", NOME: "Cabo Frio" },
+  // ICABOF4: { ID: "ICABOF4", NOME: "Cabo Frio" },
 };
 
 const CORES = {
