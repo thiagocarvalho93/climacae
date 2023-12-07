15b097ed3e2bc15c15c50854e3e9237b2b4f19d1
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-12-07 13:58:24 -0300

    🚧 Novas estações

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,7 +8,7 @@ const STATIONS = {
   IMACA7: { ID: "IMACA7", NOME: "Mirante" },
   IMACA13: { ID: "IMACA13", NOME: "Serra" },
   IMACA15: { ID: "IMACA15", NOME: "Glória" },
-  // IMACA23: { ID: "IMACA23", NOME: "" }, desativada
+  // IMACA23: { ID: "IMACA23", NOME: "LAMET" },
   IMACA26: { ID: "IMACA26", NOME: "Granja dos Cavaleiros" },
   IMACA27: { ID: "IMACA27", NOME: "Imbetiba" },
   IMACA28: { ID: "IMACA28", NOME: "Aroeira" },
@@ -16,6 +16,10 @@ const STATIONS = {
   IMACA31: { ID: "IMACA31", NOME: "Botafogo" },
   IMACA32: { ID: "IMACA32", NOME: "Visconde" },
   IMACA36: { ID: "IMACA36", NOME: "Córrego do Ouro" },
+  // IMACA40: { ID: "IMACA40", NOME: "Granja dos cavaleiros 2" },
+  IMACA41: { ID: "IMACA41", NOME: "Lagomar" },
+  IMACA42: { ID: "IMACA42", NOME: "Virgem Santa" },
+  IMACA43: { ID: "IMACA43", NOME: "Aeroporto" },
   // ICAMPO96: { ID: "ICAMPO96", NOME: "" }, desativada
   // ICAMPO223: { ID: "ICAMPO223", NOME: "" },
   // ICABOF4: { ID: "ICABOF4", NOME: "Cabo Frio" },
